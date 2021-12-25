23eeefef6bbf078fa601593b018447428e078ea2
THPNguyen <18211tt4361@gmail.com>
2021-12-25 14:10:43 +0700

    update file

--- wp-content/themes/simple/page.php
+++ wp-content/themes/simple/page.php
@@ -25,32 +25,25 @@
         </div>
         <?php wp_nav_menu($args); ?>
     </nav>
-    <div class="contaner">
+    <div class="container">
         <div class="main">
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
                     <article class="post">
                         <h3>
-                            <a href="<?php the_permalink(); ?>"> <?php the_title() ?></a>
+                            <?php the_title(); ?>
                         </h3>
-                        <div class="meta">
-                            |Created By <?php the_author() ?> on <?php the_time('F j, Y g:i a') ?>
-                        </div>
                         <?php if (has_post_thumbnail()) : ?>
                             <div class="post-thumbnail">
                                 <?php the_post_thumbnail(); ?>
                             </div>
                         <?php endif; ?>
-                        <?php the_excerpt() ?>
+                        <?php the_content(); ?>
                         <br>
-                        <a class="button" href="<?php the_permalink(); ?>">
-                            Read More
-                        </a>
                     </article>
-
                 <?php endwhile; ?>
-            <?php else :  ?>
-                <?php echo wpautop('Sory, No posts were found'); ?>
+            <?php else : ?>
+                <?php echo wpautop('Sorry, No posts were found.'); ?>
             <?php endif; ?>
         </div>
         <div class="sidebar">
